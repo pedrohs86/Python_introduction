327503930d0c3c5829bd1efdb049b49cfe12749b
Pedro Henrique dos Santos Iunes <pedro.iunes@ITB00023.itbam.org.br>
2019-08-23 13:29:18 -0400

    Start git

---
+++ hello.py
@@ -0,0 +1,30 @@
+#-*- coding: utf-8 -*-
+# coment test
+mensagem = "eae mano"
+# print ("Hello world!")
+# print ('Olá mundo!')
+
+""" 
+    comentario teste
+"""
+# print ( 2**2 ) #potência
+# print ( 10%3 ) #Resto da divisão
+# print (mensagem)
+
+var1 = 1
+var2 = 1.1
+var3 = 'string'
+var4 = True
+
+x = 2
+y = 3
+
+print (x == y)
+print (x < y)
+print (x>y)
+
+x = 3
+
+print (x==y)
+print (x < y)
+print (x>y)
